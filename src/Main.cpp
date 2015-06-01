831c239754f6810f1059613d218c96a453ece169
Jerzy Baran <1chmsid@gmail.com>
2015-06-01 14:30:20 +0100

    Can now list files recursively

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <string>
+
+using std::string;
+
+void listFiles(const char*, string);
 
 int main(){
 
+	// struct dirent *entry;
+	// DIR *dir;
+	// dir = opendir(".");
+
+	// while((entry = readdir(dir)) != nullptr)
+	// 	printf("%s %i %i\n", entry->d_name, entry->d_type, entry->d_reclen);
+
+	listFiles(".", "");
+
 	return 0;
+}
+
+void listFiles(const char* name, string indentation){
+
+	struct dirent *entry;
+	DIR *dir;
+	dir = opendir(name);
+
+	while((entry = readdir(dir)) != nullptr){
+
+		string filename = entry->d_name;
+
+		if(filename != "." && filename != ".."){
+
+			printf("%s%s\n", indentation.c_str(), filename.c_str());
+			if(entry->d_type == 4){
+				string nextPath = string(name) + "/" + filename;
+				listFiles(nextPath.c_str(), indentation + "  ");
+			}
+		}
+	}
 }
